7d7f2f6e80db7e7d1b82ce3127e6697899ba65ef
davidknoppers <david.knoppers@holbertonschool.com>
2017-04-17 20:18:31 +0000

    first draft of 1

---
+++ 0x17-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Requests username and tasks from JSON Placeholder
+based on userid (which is sys.argv[1])
+and writes it to a csv
+"""
+import sys
+import csv
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users"
+    if sys.argv[1] and int(sys.argv[1]):
+        user_id = sys.argv[1]
+        name = requests.get("{}/{}".format(
+            url, user_id)).json().get("username")
+        r = requests.get("{}/{}/todos".format(
+            url, user_id)).json()
+        with open("{}.csv".format(user_id), mode="w") as f:
+            result = csv.writer(f, delimiter=",",
+                                quotechar='"', quoting=csv.QUOTE_ALL)
+            for task in r:
+                temp = [user_id, name, task.get("completed"),
+                        task.get("title")]
+                result.writerow(temp)
