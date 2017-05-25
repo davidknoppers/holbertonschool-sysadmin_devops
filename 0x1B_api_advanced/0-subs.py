573d0871435ac4ce2ef43155e567c8c894843bba
davidknoppers <david.knoppers@holbertonschool.com>
2017-05-25 21:06:48 +0000

    updating docs and pep8

--- 0x1B_api_advanced/0-subs.py
+++ 0x1B_api_advanced/0-subs.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
+"""
+Queries the Reddit API
+Gets the whole json for the subreddit
+Then returns the subscriber count from the json object
+"""
 
 import requests
 import sys
 
 
 def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API
+    Gets the whole json for the subreddit
+    Then returns the subscriber count from the json object
+    """
     if subreddit is None:
         return 0
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
