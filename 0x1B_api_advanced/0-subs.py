f82f8a56afe6819ebda31ab70c092c898f5e01ee
davidknoppers <david.knoppers@holbertonschool.com>
2017-05-25 20:30:49 +0000

    fixed pep8

--- 0x1B_api_advanced/0-subs.py
+++ 0x1B_api_advanced/0-subs.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
-import sys
 import requests
-"https://www.reddit.com/r/askreddit/about.json"
+import sys
+
+
 def number_of_subscribers(subreddit):
     if subreddit is None:
         return 0
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    r = requests.get(url, headers={'user-agent':'Chrome'})
+    r = requests.get(url, headers={'user-agent': 'Chrome'})
     if r.status_code is not 200:
         return 0
     if r.json()['kind'] == 'Listing':
