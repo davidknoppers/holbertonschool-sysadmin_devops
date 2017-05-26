47e378050c828adaf692548ad204f3ae62a1962f
davidknoppers <david.knoppers@holbertonschool.com>
2017-05-26 18:41:09 +0000

    scrounging for last check

--- 0x1B-api_advanced/1-top_ten.py
+++ 0x1B-api_advanced/1-top_ten.py
@@ -8,27 +8,26 @@ We loop through the deep dictionary returned as a JSON and print the titles
 import requests
 
 
-def top_ten(subreddit=None):
+def top_ten(subreddit):
     """
     Queries the Reddit API
     And gets the top ten hot posts for a subreddit, if possible
     We loop through the deep dictionary returned as a JSON and print the titles
     """
-    if subreddit is None:
-        return None
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     r = requests.get(url, headers={'user-agent': 'Chrome',
                                    'allow_redirects': 'False'})
-    if r.status_code != 200:
-        print ("None")
-        return None
-    children = r.json()['data']['children']
-    if children == []:
-        print("None")
+    if r.status_code == 200:
+        children = r.json()['data']['children']
+        if children == []:
+            print("None")
+            return
+        else:
+            #counter needed because we might have more than ten results!
+            counter = 0
+            for child in children:
+                if counter < 10:
+                    print(child['data']['title'])
+                counter += 1
     else:
-        #counter needed because we might have more than ten results!
-        counter = 0
-        for child in children:
-            if counter < 10:
-                print(child['data']['title'])
-            counter += 1
+        print("None")
