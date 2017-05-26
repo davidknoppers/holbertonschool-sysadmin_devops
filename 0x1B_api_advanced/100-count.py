096b8340cd33f3027196a53f0b3a55afabec26e6
davidknoppers <david.knoppers@holbertonschool.com>
2017-05-26 03:47:48 +0000

    first janky draft of 100

---
+++ 0x1B_api_advanced/100-count.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API
+And gets all hot posts for a subreddit, if possible
+Takes in a list of words to track
+Traverses the results recursively to return a count for each word in the list
+"""
+
+import requests
+import sys
+
+
+def count_words(subreddit, word_list=[], count_dict={}, after=""):
+    """
+    Queries the Reddit API
+    And gets all hot posts for a subreddit, if possible
+    Traverses the results recursively to return counts for every term
+    in word_list
+    """
+    if subreddit is None or len(word_list) == 0:
+        return None
+    if len(count_dict) == 0:
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    else:
+        url = "https://www.reddit.com/r/{}/hot.json?limit=500&after={}".format(
+            subreddit, after)
+    r = requests.get(url, headers={'user-agent': 'Chrome'})
+    if r.status_code is not 200:
+        return None
+    children = r.json()['data']['children']
+    if count_dict == {}:
+        for word in word_list:
+            count_dict[word.lower()] = 0
+    for child in children:
+        temp = child['data']['title'].split(' ')
+        for substring in temp:
+            if substring.lower() in word_list:
+                count_dict[substring.lower()] += 1
+
+    current_after = r.json()['data']['after']
+
+    if current_after is None:
+        for key in count_dict.keys():
+            if count_dict[key] > 0:
+                print("{}: {}".format(key, count_dict[key]))
+        return
+    return count_words(subreddit, word_list, count_dict, current_after)
