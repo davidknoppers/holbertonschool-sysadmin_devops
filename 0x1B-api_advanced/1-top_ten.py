da28e020d9ceae86550d13d3dba5507dca27e71c
davidknoppers <david.knoppers@holbertonschool.com>
2017-05-26 17:33:53 +0000

    wrangling the error handling

--- 0x1B-api_advanced/1-top_ten.py
+++ 0x1B-api_advanced/1-top_ten.py
@@ -6,7 +6,6 @@ We loop through the deep dictionary returned as a JSON and print the titles
 """
 
 import requests
-import sys
 
 
 def top_ten(subreddit=None):
@@ -18,15 +17,13 @@ def top_ten(subreddit=None):
     if subreddit is None:
         return None
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    r = requests.get(url, headers={'user-agent': 'Chrome'})
+    r = requests.get(url, headers={'user-agent': 'Chrome',
+                                   'allow_redirects': 'False'})
     if r.status_code != 200:
         return None
     children = r.json()['data']['children']
     if children == []:
-        return None
-    for child in children:
-        print(child['data']['title'])
-"""
-if len(sys.argv) > 1:
-    subreddit = sys.argv[1]
-"""
+        print("None")
+    else:
+        for child in children:
+            print(child['data']['title'])
